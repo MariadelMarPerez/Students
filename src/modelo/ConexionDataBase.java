16f6c38a070e0fa20f484f12e9509c4d9d03ab71
MariadelMarPerez <mariadelmarperezmontengro@gmail.com>
2025-05-24 09:42:37 -0500

    Primer Commit

---
+++ src/modelo/ConexionDataBase.java
@@ -0,0 +1,28 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDataBase {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/Students";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1105928204"; 
+
+    private static Connection conexion;
+
+    public static Connection getConexion() {
+        if (conexion == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Conexión exitosa a la base de datos.");
+            } catch (ClassNotFoundException | SQLException e) {
+                System.err.println("Error en la conexión: " + e.getMessage());
+            }
+        }
+        return conexion;
+    }
+}
+
+
